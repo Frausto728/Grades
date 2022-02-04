031ecdb7ea92a2ffe643dd2d353e98978b10343c
Luis Frausto <luisfrausto728@yahoo.com>
2022-02-03 20:33:24 -0600

    Code for grade letter

---
+++ Grades.py
@@ -0,0 +1,29 @@
+def average_grades(grades):
+    length = len(grades)
+    total_grades = sum(grades)
+    average = round(total_grades / length)
+    return average
+
+def grade_letter(average):
+    if average >= 90:
+        grade = "A"
+    elif average >= 80:
+        grade = "B"
+    elif average >= 70:
+        grade = "C"
+    elif average >= 60:
+        grade = "D"
+    else:
+        grade = "F"
+    return grade
+
+grades = []
+for i in range(3):
+    score = int(input("Enter grade numbers: "))
+    grades.append(score)
+
+average = average_grades(grades)
+letter = grade_letter(average)
+
+print("Your average is" , average)
+print("Your letter grade is" , letter)
